ae551de31323578a622df89d5a4da35583af8006
Hilton2245 <129993778+Hilton2245@users.noreply.github.com>
2023-10-27 10:34:15 -0300

    Add files via upload

---
+++ teste.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class teste{
+    public static void main(String[] args){
+        Scanner scan= new Scanner (System in);
+        System.out.println("Olá, qual seu nome?");
+        String nome = scan.nextLine();
+        System.out.println("Ahhhh"+ nome);
+    }
+}
